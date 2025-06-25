5675eaa92c5d8674eaa3bf3e83d38dcaa393f85e
AadityaRaj77 <aaditya200602@gmail.com>
2025-06-25 06:06:53 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,24 @@
 // import './App.css'
-import Home from './components/Home';
-import Whiteboard from './components/Whiteboard';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "./components/Home.jsx";
+import Whiteboard from "./components/Whiteboard.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home/>
+      element: <Home />,
     },
     {
       path: "/whiteboard",
-      element: <Whiteboard/>
-    }
-  ])
+      element: <Whiteboard />,
+    },
+  ]);
   return (
     <>
-    <RouterProvider router={router}>
-
-    </RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
